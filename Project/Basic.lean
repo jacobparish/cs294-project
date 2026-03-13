dcbea5a01046f556a36970c9ef8eb34fe49da472
Jacob Parish <jacobsparish@gmail.com>
2026-03-13 10:28:19 -0700

    Update Basic.lean

--- Project/Basic.lean
+++ Project/Basic.lean
@@ -1,1 +1,7 @@
-def hello := "world"
+import Mathlib.Computability.Halting
+import Mathlib.Computability.TuringDegree
+
+open Nat.Partrec
+
+/-- The halting problem -/
+def halts_on_zero : Code → Prop := fun c => (c.eval 0).Dom
