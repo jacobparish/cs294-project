d6c6a8ee4f7619062eb8c1b9efdc891280bb5113
Jacob Parish <jacobsparish@gmail.com>
2026-04-07 15:50:15 -0700

    Delete Basic.lean and add other files to Project.lean

--- Project/Basic.lean
+++
@@ -1,7 +0,0 @@
-import Mathlib.Computability.Halting
-import Mathlib.Computability.TuringDegree
-
-open Nat.Partrec
-
-/-- The halting problem -/
-def halts_on_zero : Code → Prop := fun c => (c.eval 0).Dom
